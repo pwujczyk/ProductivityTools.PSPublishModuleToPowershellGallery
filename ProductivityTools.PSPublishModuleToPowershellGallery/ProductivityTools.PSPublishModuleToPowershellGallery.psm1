864270e74f7cebf3494372fd37b07d57aad419e7
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-29 23:06:52 +0200

    Automatic Commit

--- ProductivityTools.PSPublishModuleToPowershellGallery/ProductivityTools.PSPublishModuleToPowershellGallery.psm1
+++ ProductivityTools.PSPublishModuleToPowershellGallery/ProductivityTools.PSPublishModuleToPowershellGallery.psm1
@@ -3,7 +3,7 @@ function Publish-ModuleToPowershellGallery {
 	[CmdletBinding()]
 	param()
 	
-	$repository=Get-PSRepository |where { $_.SourceLocation -eq "https://www.powershellgallery.com/api/v2/"}
+	$repository=Get-PSRepository |where { $_.SourceLocation -eq "https://www.powershellgallery.com/api/v2"}
 	$repositoryName=$repository.Name
 	Publish-ModuleTo -PSRepository $repositoryName -PSRepositoryApiKey "PSRepositoryApiKey" -Verbose:$VerbosePreference
 }
